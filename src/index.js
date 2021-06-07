0b040d5da1bae05e2ea304d5d2aa1b9b26f37483
Fairfaxx <fede.lemaire@gmail.com>
2021-06-07 20:53:14 -0300

    erased files and set the base url for axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseUrl = "http://rem-rest-api.herokuapp.com/api";
+console.log(axios.defaults.baseUrl)
 ReactDOM.render(
   <React.StrictMode>
     <App />
